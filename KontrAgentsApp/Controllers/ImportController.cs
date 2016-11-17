9eb8e4a6854a30d42bede25b0a72f353e6283178
Дарья <@DASHA-HP>
2016-11-17 14:25:09 +0400

    Первая версия с выводом найденных в файле контрагентов на форму

--- KontrAgentsApp/Controllers/ImportController.cs
+++ KontrAgentsApp/Controllers/ImportController.cs
@@ -5,33 +5,82 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using KontrAgentsApp.Models;
 
 namespace KontrAgentsApp.Controllers
 {
     public class ImportController : ApiController
     {
-        public async Task<IHttpActionResult> Post()
+        private List<KontrAgent> listKontrAgents;
+
+        public async Task<IEnumerable<KontrAgent>> Post()
         {
-            if (!Request.Content.IsMimeMultipartContent())
-            {
-                return BadRequest();
-            }
-            var provider = new MultipartMemoryStreamProvider();
-            //путь к папке на сервере
-            string root = System.Web.HttpContext.Current.Server.MapPath("~/Files/");
-            await Request.Content.ReadAsMultipartAsync(provider);
+            string line;
+            KontrAgent tempKontrAgent = null;
 
-            foreach (var file in provider.Contents)
+            try
             {
-                var filename = file.Headers.ContentDisposition.FileName.Trim('\"');
-                byte[] fileArray = await file.ReadAsByteArrayAsync();
-
-                using (System.IO.FileStream fs = new System.IO.FileStream(root + filename, System.IO.FileMode.Create))
+                if (!Request.Content.IsMimeMultipartContent())
                 {
-                    await fs.WriteAsync(fileArray, 0, fileArray.Length);
+                    return null;
                 }
+                var provider = new MultipartMemoryStreamProvider();
+                //путь к папке на сервере
+                string root = System.Web.HttpContext.Current.Server.MapPath("~/Files/");
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                listKontrAgents = new List<KontrAgent>();
+
+                foreach (var file in provider.Contents)
+                {
+                    var filename = file.Headers.ContentDisposition.FileName.Trim('\"');
+                    byte[] fileArray = await file.ReadAsByteArrayAsync();
+
+                    using (FileStream fs = new FileStream(root + filename, FileMode.Create))
+                    {
+                        await fs.WriteAsync(fileArray, 0, fileArray.Length);
+                    }
+                    
+
+                    //чтение из файла с созданием списка моделей
+                    using (StreamReader sr = new StreamReader(root + filename, Encoding.GetEncoding("windows-1251")))
+                    {
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (line.StartsWith("ПолучательИНН="))
+                            {
+                                tempKontrAgent = new KontrAgent();
+                                tempKontrAgent.Inn = line.Replace("ПолучательИНН=", "");
+                            }
+                            else if (line.StartsWith("Получатель1="))
+                            {
+                                tempKontrAgent.Name = line.Replace("Получатель1=", "");
+                            }
+                            else if (line.StartsWith("Получатель2="))
+                            {
+                                tempKontrAgent.Account = line.Replace("Получатель2=", "");
+                            }
+                            else if (line.StartsWith("Получатель3="))
+                            {
+                                tempKontrAgent.BankName = line.Replace("Получатель3=", "");
+                            }
+                            else if (line.StartsWith("Получатель4="))
+                            {
+                                tempKontrAgent.BankCity = line.Replace("Получатель4=", "");
+                                listKontrAgents.Add(tempKontrAgent);
+                            }
+                        }
+                    }
+                    File.Delete(root + filename);
+                }
+                return listKontrAgents;
             }
-            return Ok("Файл успешно загружен. Найденные контрагенты отображены на странице");
+            catch(Exception ex)
+            {
+                return null;
+            }
         }
     }
 }
